feb84dab39d3eb22ad2bc78e75dd12a34e9c312c
kentarohosobuchi <kentaro.khoso@gmail.com>
2020-08-15 08:53:10 +0900

    changed_some_views

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,10 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1500 }
   # validates :image, presence: true
+  mount_uploader :image, ImageUploader
 
   belongs_to :user
   has_many :comments
-
-  mount_uploader :image, ImageUploader
 
   def self.search(search)
     if search
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_033457) do
+ActiveRecord::Schema.define(version: 2020_08_14_054805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.text "image"
+    t.string "title", null: false
+    t.text "description", null: false
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
