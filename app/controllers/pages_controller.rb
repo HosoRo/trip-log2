bbde52b7679fb31ecb23ddc2cfd839a6b7a7423f
kentarohosobuchi <kentaro.khoso@gmail.com>
2020-08-17 04:58:59 +0900

    fixed_views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @articles = Article.includes(:user).order("created_at DESC").limit(6)
+    @articles = Article.includes(:user).order("created_at DESC").limit(5)
     # @articles = Article.includes(:article_images).order('created_at DESC')
     # @categories = Article.includes(:article_images).where(category_id:1..4).order('created_at DESC').limit(4).where.not(trading_status: 0)
   end
